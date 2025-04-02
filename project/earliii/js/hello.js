fc57b04f2940d26b62a67a9d3e6024a7f0ffbed5
troie <138965947+maolahdesign@users.noreply.github.com>
2025-04-02 13:39:37 +0800

    upadte

---
+++ project/earliii/js/hello.js
@@ -0,0 +1,49 @@
+$(document).ready(function(){
+    $('.brand button').on('click',function(){
+        var buttonId = $(this).attr("id");
+        alert(buttonId);
+        $('.project').addClass('project_on');
+       
+	});
+    
+    $('.project button').on('click',function(){
+        $('.project').removeClass('project_on');
+        
+	});
+});
+                  
+function submitForm() {
+  // 防止表單直接提交
+  event.preventDefault();
+  
+  // 獲取表單數據
+  var formData = new FormData(document.getElementById('contactForm'));
+  
+  // 使用 fetch API 發送數據
+    console.log('開始提交表單...');
+  fetch('https://script.google.com/a/macros/earliii.com/s/AKfycbxDNPygAQA8_EeN9iC-GbGPN-h8WZf-WADaZzXEBhDY_LqOlDcHGqE5Moh4EH2yQZaqSg/exec', {
+    method: 'POST',
+    body: formData
+  })
+  .then(response => {
+    console.log('收到回應:', response);
+    return response.json();
+  })
+  .then(data => {
+      console.log('處理數據:', data);
+    if(data.result === 'success') {
+      // 顯示成功訊息
+      document.getElementById('successMessage').style.display = 'block';
+      document.getElementById('contactForm').reset();
+    } else {
+      // 顯示錯誤訊息
+      alert('提交失敗: ' + data.message);
+    }
+  })
+  .catch(error => {
+    console.error('錯誤:', error);
+    alert('發生錯誤，請稍後再試');
+  });
+  
+  return false;
+}
