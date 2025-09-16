6d46510e0e10bf38e471898a2cfff1e92e1c8b2f
troie <138965947+maolahdesign@users.noreply.github.com>
2025-09-16 15:10:35 +0800

    upadte

---
+++ project/juino/js/script.js
@@ -0,0 +1,9 @@
+$(function() {
+ 
+    $(".last_chat h2").on("click", function() {
+        $('body').css('overflow','auto')
+        $(this).parent().hide();
+//        $(this).parent().addClass('hide');
+    });
+
+});
